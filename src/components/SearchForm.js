c87d29119d8c5657ca61874f93e78bea6c88f046
torsumkhan <torsum.khan@gmail.com>
2022-07-11 21:59:01 +0500

    add search bar

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { cocktailContext } from "../context";
+import "./search.css";
 
 const SearchForm = () => {
   const { setSearchTerm } = useContext(cocktailContext);
+  const searchValue = useRef("");
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="search">
-      <h1>SearchForm components</h1>
+    <div className="search-bar">
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">Search:</label>
+        <input
+          type="text"
+          id="name"
+          required
+          ref={searchValue}
+          onChange={() => setSearchTerm(searchValue.current.value)}
+        />
+      </form>
     </div>
   );
 };
